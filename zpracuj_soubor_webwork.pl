b223f115ebe09da98e0e6590e0d573b61e36dc58
Robert Marik <robert.marik.cz@gmail.com>
2020-07-10 22:40:52 +0200

    uvod

--- zpracuj_soubor_webwork.pl
+++ zpracuj_soubor_webwork.pl
@@ -4,10 +4,11 @@
 # 2. Stáhnout bodovací arch csv, uložit jako bodovaci_arch.csv
 # 3. Spustit:
 #       iconv -f CP1250 -t UTF-8 seznam_cp1250.csv -o seznam_utf8.csv
-# 4. Spustit:
+# 4. Smazat uvodni radky souboru bodovaci_arch.csv a seznam_utf8.csv
+# 5. Spustit:
 #       perl zpracuj_soubor_webwork.pl seznam_utf8.csv
-# 5. naimportovat courselist_crypted.lst do WeBWoRK
-# 6. naimportovat bodovaci_arch_import.csv do UISu
+# 6. naimportovat courselist_crypted.lst do WeBWoRK
+# 7. naimportovat bodovaci_arch_import.csv do UISu
 
 my @setA = ("B","C","D","F","G","H","J","K","L","M","N","P","R","S","T","V","Z");
 my @seta = ("b","c","d","f","g","h","j","k","l","m","n","p","r","s","t","v","z");
@@ -29,13 +30,10 @@ open(my $data, '<', $file) or die "Could not open '$file' $!\n";
 while (my $line = <$data>) {
     chomp $line;
     my @data = split /;/, $line;
-#    $heslo=$setA[rand @setA].$seth[rand @seth].$seta[rand @seta].$dig[rand @dig].$dig[rand @dig].$seta[rand @seta].$seth[rand @seth];
     $heslo=$setA[rand @setA].$seth[rand @seth].$seta[rand @seta].".".$seta[rand @seta].$seth[rand @seth];
     $zakodovane_heslo=crypt($heslo,join"",(".","/",0..9,"A".."Z","a".."z")[rand 64,rand 64]);
     $txt_kod=$txt_kod.$data[3].",".$data[1].",".$data[2].",,,,,".$data[4].",".$data[5].",".$zakodovane_heslo.",\n";
-    #$txt_plain=$txt_plain.$data[3].",".$heslo.",\n";
     $mydata{"\"".$data[5]."\""}=$heslo;
-    
 }
 
 open(FH, '>', "courselist_crypted.lst") or die $!;
@@ -43,13 +41,11 @@ print FH $txt_kod;
 close(FH);
 
 $newfile="";
-
 open(my $data, '<', "bodovaci_arch.csv") or die "Could not open file\n";
 
 while (my $line = <$data>) {
     chomp $line;
     my @data = split /,/, $line;
-    print $data[5];
     $data[4]="\"".$mydata{$data[5]}."\"";
     $newline=join (",",@data);
     $newfile=$newfile."\n".$newline;
@@ -60,6 +56,6 @@ open(FH, '>', "bodovaci_arch_import.csv") or die $!;
 print FH $newfile;
 close(FH);
 
-print "Finish";
+print "Finish\n";
 
 
