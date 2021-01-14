d00910990fc3689bf28d7294bae68560d16a4eda
robert-marik <marik@mendelu.cz>
2021-01-14 10:16:45 +0100

    PGessayLivePreview.pl

---
+++ makra/PGessayLivePreview.pl
@@ -0,0 +1,121 @@
+
+HEADER_TEXT(<<EOF);
+
+<script>
+var Preview = {
+  delay: 150,        // delay after keystroke before updating
+
+  preview: null,     // filled in by Init below
+  buffer: null,      // filled in by Init below
+
+  timeout: null,     // store setTimout id
+  mjRunning: false,  // true when MathJax is processing
+  mjPending: false,  // true when a typeset has been queued
+  oldText: null,     // used to check if an update is needed
+
+  //
+  //  Get the preview and buffer DIV's
+  //
+  Init: function () {
+    this.preview = document.getElementById("MathPreview");
+    this.buffer = document.getElementById("MathBuffer");
+  },
+
+  //
+  //  Switch the buffer and preview, and display the right one.
+  //  (We use visibility:hidden rather than display:none since
+  //  the results of running MathJax are more accurate that way.)
+  //
+  SwapBuffers: function () {
+    var buffer = this.preview, preview = this.buffer;
+    this.buffer = buffer; this.preview = preview;
+    buffer.style.visibility = "hidden"; buffer.style.position = "absolute";
+    preview.style.position = ""; preview.style.visibility = "";
+  },
+
+  //
+  //  This gets called when a key is pressed in the textarea.
+  //  We check if there is already a pending update and clear it if so.
+  //  Then set up an update to occur after a small delay (so if more keys
+  //    are pressed, the update won't occur until after there has been 
+  //    a pause in the typing).
+  //  The callback function is set up below, after the Preview object is set up.
+  //
+  Update: function () {
+    if (this.timeout) {clearTimeout(this.timeout)}
+    this.timeout = setTimeout(this.callback,this.delay);
+  },
+
+  //
+  //  Creates the preview and runs MathJax on it.
+  //  If MathJax is already trying to render the code, return
+  //  If the text hasn't changed, return
+  //  Otherwise, indicate that MathJax is running, and start the
+  //    typesetting.  After it is done, call PreviewDone.
+  //  
+  CreatePreview: function () {
+    Preview.timeout = null;
+    if (this.mjPending) return;
+    var text = document.getElementById("AnSwEr0001").value;
+    if (text === this.oldtext) return;
+    if (this.mjRunning) {
+      this.mjPending = true;
+      MathJax.Hub.Queue(["CreatePreview",this]);
+    } else {
+      this.buffer.innerHTML = this.oldtext = text;
+      this.mjRunning = true;
+      MathJax.Hub.Queue(
+	["Typeset",MathJax.Hub,this.buffer],
+	["PreviewDone",this]
+      );
+    }
+  },
+
+  //
+  //  Indicate that MathJax is no longer running,
+  //  and swap the buffers to show the results.
+  //
+  PreviewDone: function () {
+    this.mjRunning = this.mjPending = false;
+    this.SwapBuffers();
+  }
+
+};
+
+//
+//  Cache a callback to the CreatePreview action
+//
+Preview.callback = MathJax.Callback(["CreatePreview",Preview]);
+Preview.callback.autoReset = true;  // make sure it can run more than once
+
+function Viditelnost()
+{
+Preview.Init();
+jQuery("#AnSwEr0001").css("width","100%");
+jQuery("#AnSwEr0001").css("box-sizing","border-box");
+jQuery("#AnSwEr0001").css("height","200px");
+jQuery("#AnSwEr0001").css("display","inline-block");
+Preview.Update();
+jQuery("#obalkaPreview").toggle();
+}
+
+</script>
+
+<style>
+#MathPreview, #MathBuffer  
+{
+background-color: #ffd633;
+padding:10px;
+width:100%;
+height:auto;
+box-sizing: border-box;
+display: inline-block;
+}
+
+
+#obalkaPreview{display:none;}
+</style>
+
+EOF
+
+$tlacitko='<div id="obalkaPreview"><div id="MathPreview"></div><div id="MathBuffer"></div></div><script>jQuery("#AnSwEr0001").keyup(function() {Preview.Update();});</script><br> <a class="btn btn-primary btn-essay" onclick="Viditelnost();">Živý náhled Zapnout/Vypnout</a>' ;
